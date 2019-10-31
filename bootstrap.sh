74c43ec3f6dd6e525ab4ef4021cd2c2edf31840e
Albert Robin Moriarty <19686802+armoriarty@users.noreply.github.com>
2019-10-30 20:28:08 -0400

    Added tmux dotfile
    This commit will modify tmux keyboard shortcut
    to prevent interference with vim shortcuts
    
    A new tmux.conf file was added and the bootstrap
    script was updated with a coresponding link for tmux.conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,3 +12,4 @@ ln -s ${PWD}/vimrc ~/.vimrc
 ln -s ${PWD}/gitignore ~/.gitignore
 ln -s ${PWD}/gitconfig ~/.gitconfig
 ln -s ${PWD}/git_commit_template.txt ~/.git_commit_template.txt
+ln -s ${PWD}/tmux.conf ~/.tmux.conf
